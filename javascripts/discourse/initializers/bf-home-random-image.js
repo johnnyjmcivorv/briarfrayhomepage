6546f73c4eb5ca6f18a6ce57c8ee902fdab909f3
johnnyjmcivorv <46433372+johnnyjmcivorv@users.noreply.github.com>
2026-01-17 23:44:25 -0500

    Create bf-home-random-image.js

---
+++ javascripts/discourse/initializers/bf-home-random-image.js
@@ -0,0 +1,18 @@
+import { apiInitializer } from "discourse/lib/api";
+
+const IMAGES = [
+  "https://i.imgur.com/QopcbDo.jpg",
+  "https://i.imgur.com/RQEMEV6.jpg",
+];
+
+export default apiInitializer((api) => {
+  api.onPageChange((url) => {
+    if (url !== "/") return;
+
+    const img = document.getElementById("bf-home-random-img");
+    if (!img) return;
+
+    const pick = IMAGES[Math.floor(Math.random() * IMAGES.length)];
+    img.src = pick;
+  });
+});
