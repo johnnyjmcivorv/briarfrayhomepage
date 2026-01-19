0f1fc20d32ba2b8677acaec9e32d696f9cf7a25e
johnnyjmcivorv <46433372+johnnyjmcivorv@users.noreply.github.com>
2026-01-19 11:23:47 -0500

    Update bf-home-random-image.js
    Removed Miss Minnie from the image cycler.

--- javascripts/discourse/initializers/bf-home-random-image.js
+++ javascripts/discourse/initializers/bf-home-random-image.js
@@ -4,7 +4,6 @@ const IMAGES = [
   "https://i.imgur.com/QopcbDo.jpg",
   "https://i.imgur.com/RQEMEV6.jpg",
   "https://i.imgur.com/Yza7azn.png",
-  "https://i.imgur.com/xN1oJK2.gif",
   "https://i.imgur.com/iIVkzqo.png",
 ];
 
